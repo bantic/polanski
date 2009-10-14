178a3a4f30cf60dc8c6a9a12ea3eae3e1e7fd33c
Cory <cory.forsyth@gmail.com>
2009-10-14 10:34:17 -0400

    fix voting behavior

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
-  cache_sweeper :vote_sweeper, :only => [:create]
-  caches_page :index
+  # cache_sweeper :vote_sweeper, :only => [:create]
+  # caches_page :index
   
   def new
     @vote = Vote.new
@@ -9,7 +9,7 @@ class VotesController < ApplicationController
   def create
     @vote = Vote.new(params[:vote].merge({:ip => request.remote_ip}))
     @vote.save # ignore errors
-    
+    flash[:vote] = @vote.positive? ? "JAIL" : "FREE"
     redirect_to votes_path
   end
   
